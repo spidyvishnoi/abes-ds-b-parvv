a50873d872bccd1d2ae2bcd9b984b54727e6912c
spidyvishnoi <parvvishnoi893@gmail.com>
2025-11-07 15:32:19 +0530

    update2

--- frontend/vite-project/src/components/Link1.jsx
+++ frontend/vite-project/src/components/Link1.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import{Routes,Link,Route} from 'react-router-dom'
+function Home(){
+    return <h1>This is my home page</h1>
 
+}
+function About(){
+    return <h1>This is my about page</h1>
+}
 function Link1() {
   return (
     <div>
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+
+           
+
+        </nav>
         <Routes>
             <Route  path="/"element={<Home/>}/>
             <Route  path="/about"element={<About/>}/>
